ac1ec97e9d7f4760d81413659ce95c98c1cff168
”hirukawa <hiru0621@gmail.com>
2020-05-13 06:47:58 +0000

    Rails 05 ログイン機能の実装/カリキュラム/課題

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   get 'pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -7,5 +8,9 @@ Rails.application.routes.draw do
   get "pages/help"
   
   resources :users
+  
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   
 end
